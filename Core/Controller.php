6f75d456a18101246ca726b2c848e87fc4f23642
Filipe Natanael <filipenatanael@live.com>
2017-04-09 23:52:28 -0300

    Update Core
    Update file core

--- Core/Controller.php
+++ Core/Controller.php
@@ -3,6 +3,7 @@
 class Controller{
 
    public function loadView($viewName, $viewData = array()){
+      extract($viewData);
       include_once 'Views/'.$viewName.'.php';
    }
 
--- Core/Core.php
+++ Core/Core.php
@@ -2,28 +2,33 @@
 
 class Core{
 
-   private $currentController;
-   private $currentAction;
+   //private $currentController;
+   //private $currentAction;
 
    public function run(){
      $url = substr($_SERVER['PHP_SELF'],22);
-     
+
      if(!empty($url)){
        $url = explode('/',$url);
        array_shift($url);
-       $this->currentController = $url[0].'Controller';
+       $currentController = $url[0].'Controller';
 
        if(isset($url[1])){
-       $this->currentAction = $url[1];
+       $currentAction = $url[1];
        }else{
-       $this->currentAction = 'index';
+       $currentAction = 'index';
        }
      }else {
-       $this->currentController = 'homeController';
-       $this->currentAction = 'index';
+       $currentController = 'homeController';
+       $currentAction = 'index';
      }
-     echo 'Controller: '.$this->currentController.'<br>';
-     echo 'Action: '.$this->currentAction;
+     //---------------------------------------------
+     require_once 'Core/Controller.php';
+     $control = new $currentController();
+     $control->$currentAction();
+
+     echo '</br></br>'.'Controller: '.$currentController.'<br>';
+     echo 'Action: '.$currentAction;
 
    }
 
