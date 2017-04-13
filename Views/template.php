972b433f6b8f04e319ac5f6f80a20126dd81928b
Filipe Natanael <filipenatanael@live.com>
2017-04-13 14:03:31 -0300

    htaccess
    I changed htaccess

--- Views/template.php
+++ Views/template.php
@@ -6,7 +6,9 @@
   </head>
   <body>
     <h3>Topo do sistema!</h3>
-    .....
+     <?php
+     $this->loadviewInTemplate($viewName, $viewData);
+     ?>
     <h3>Rodapé do sistema!</h3>
   </body>
 </html>
